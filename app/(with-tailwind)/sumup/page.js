c337ca69037fb5e8f0a697da74011193039fb2e3
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2026-07-14 10:08:17 +0000

    fix node issue

--- app/(with-tailwind)/sumup/page.js
+++
@@ -1,9 +0,0 @@
-import StaticPage from '@/components/StaticPage';
-
-export const metadata = {
-  title: 'Sumup',
-};
-
-export default function Page() {
-  return <StaticPage sourcePath="sumup/index.html" title="Sumup" />;
-}
